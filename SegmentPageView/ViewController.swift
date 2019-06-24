6722d5e0e83e4e56ad1fa46ea33d532f6c0dfdd4
Mazy <mazy_ios@163.com>
2019-06-24 18:08:50 +0800

    init repo

---
+++ SegmentPageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentPageView
+//
+//  Created by Mazy on 2019/6/24.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
